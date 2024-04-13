5d774555d6e1e75cae2c059fc1ff292ca5a647b8
Vladimir Li <vladde1991@gmail.com>
2024-04-13 12:15:38 +0200

    feat: add react query

--- frontend/app/ui/auth/SignUp.tsx
+++ frontend/app/ui/auth/SignUp.tsx
@@ -1,6 +1,8 @@
-import { createUser } from "@/app/lib/actions";
+import axios from "@/app/axios";
 import { Button } from "@/app/ui/common/Button";
 import { Input } from "@/app/ui/common/Input";
+import { AxiosError } from "axios";
+import { useMutation } from "react-query";
 import { PasswordInput } from "../common/PasswordInput";
 
 interface SignUpProps {
@@ -8,19 +10,51 @@ interface SignUpProps {
 }
 
 export function SignUp(props: SignUpProps) {
+  const signUpMutation = useMutation(
+    (data: { email: string; password: string }) =>
+      axios.post("/users", data).then((res) => res.data)
+  );
+
+  const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    signUpMutation.mutate({ email, password });
+  };
+
+  const isMutating = signUpMutation.isLoading;
+
+  // TODO: Handle mutation error
+  const mutationError = (signUpMutation.error as AxiosError)?.response?.data;
+  console.log({ mutationError });
+
   return (
     <>
       <h1 className="font-bold text-center">Sign Up</h1>
-      <form className="mt-2" action={createUser}>
+      <form className="mt-2" onSubmit={handleSignUp}>
         <label htmlFor="email" className="block">
           Email
         </label>
-        <Input type="email" placeholder="Email" id="email" name="email" />
+        <Input
+          type="email"
+          placeholder="Email"
+          id="email"
+          name="email"
+          disabled={isMutating}
+        />
         <label htmlFor="password" className="block mt-2">
           Password
         </label>
-        <PasswordInput placeholder="Password" id="password" name="password" />
-        <Button className="mt-4">Sign Up</Button>
+        <PasswordInput
+          placeholder="Password"
+          id="password"
+          name="password"
+          disabled={isMutating}
+        />
+        <Button className="mt-4" disabled={isMutating}>
+          Sign Up
+        </Button>
       </form>
       <p className="mt-4">
         Already have an account?{" "}
---
+++ frontend/app/ui/common/Snackbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+interface SnackbarProps extends React.ComponentPropsWithoutRef<"div"> {
+  open: boolean;
+  onClose?: () => void;
+  autoHideDuration?: number;
+}
+
+export function Snackbar(props: SnackbarProps) {
+  const { open, onClose, autoHideDuration, ...rest } = props;
+  const [show, setShow] = React.useState(open);
+
+  React.useEffect(() => {
+    setShow(open);
+  }, [open]);
+
+  React.useEffect(() => {
+    if (show && autoHideDuration) {
+      const timeout = setTimeout(() => {
+        setShow(false);
+        onClose && onClose();
+      }, autoHideDuration);
+      return () => clearTimeout(timeout);
+    }
+  }, [show, autoHideDuration, onClose]);
+
+  return (
+    <div
+      className={`fixed bottom-4 left-4 w-full max-w-sm p-4 bg-white shadow-md rounded-md transition-transform transform ${
+        show ? "translate-x-0" : "translate-x-full"
+      }`}
+      {...rest}
+    >
+      {props.children}
+    </div>
+  );
+}
